2f48b936d60fb38eafa7ff9268eb37a6b26b4497
mihaneyano <131777676+mihaneyano@users.noreply.github.com>
2023-05-07 17:03:58 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get'top' => 'homes#top'
-  get'books' => 'homes#books'
+  get'books' => 'homes#new'
+  post'books' => 'homes#create'
 end
