b0b636ef75000eae2d65f0ab111ec1c0ccaca669
mihaneyano <131777676+mihaneyano@users.noreply.github.com>
2023-05-08 14:14:17 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book)
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -20,10 +21,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path(book)
+    redirect_to books_path(book)
+    flash[:notice] = "Book was successfully destroyed."
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get'top' => 'homes#top'
+  get'/' => 'homes#top'
   resources :books
+  patch 'book/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
 end
